3a69586a02ccb5b5b601ee2e4c6c2f4948838183
pragathiswaar <pragathiswaar@gmail.com>
2025-04-03 13:55:32 +0530

    Defined GarbageCollector and AllocationMap structs

--- gc.h
+++ gc.h
@@ -1,1 +1,19 @@
+#ifndef __GC_H__
+#define __GC_H__
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+struct AllocationMap;
+
+typedef struct GarbageCollector{
+    struct AllocationMap* allocs;
+    bool paused;
+    void* bos; 
+    size_t min_size;
+}GarbageCollector;
+
+extern GarbageCollector gc;
+
+#endif
