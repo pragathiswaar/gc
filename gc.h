ce59f1373dceb528d075d5e9e114535aa4d9e12d
pragathiswaar <pragathiswaar@gmail.com>
2025-04-03 14:02:20 +0530

    Declared core GC functions

--- gc.h
+++ gc.h
@@ -16,4 +16,11 @@ typedef struct GarbageCollector{
 
 extern GarbageCollector gc;
 
+void gc_start(GarbageCollector* gc,void* bos);
+void gc_start_ext(GarbageCollector* gc,void* bos,size_t initial_size,size_t min_size,double downsize_load_factor,double upsize_load_factor,double sweep_factor);
+size_t gc_stop(GarbageCollector* gc);
+void gc_pause(GarbageCollector* gc);
+void gc_resume(GarbageCollector* gc);
+size_t gc_run(GarbageCollector* gc);
+
 #endif
